c2c7ab22c32a8f24eb2ac31a44767062d14d543d
Issaoui Rami <ramiissaoui55@gmail.com>
2020-07-28 15:55:13 +0100

    Updates

---
+++ precache-manifest.268edfb3885edd7c7889fb97f4198920.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf146c3f4232e843732db3821c78e262",
+    "url": "/react-admin/index.html"
+  },
+  {
+    "revision": "11c14ddbd43fb0288584",
+    "url": "/react-admin/static/css/main.aa0ae27d.chunk.css"
+  },
+  {
+    "revision": "be6c22cfdd271123c9d8",
+    "url": "/react-admin/static/js/2.2dae1109.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-admin/static/js/2.2dae1109.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11c14ddbd43fb0288584",
+    "url": "/react-admin/static/js/main.3ad7133b.chunk.js"
+  },
+  {
+    "revision": "61c7e3f14c2d06205442",
+    "url": "/react-admin/static/js/runtime-main.dc242563.js"
+  }
+]);
